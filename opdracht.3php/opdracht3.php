62a1de79ec10f715646e0e2bfddc848891fcf5e9
MT-97 <144113816+MT-97@users.noreply.github.com>
2023-11-21 11:28:12 +0100

    Add files via upload

---
+++ opdracht.3php/opdracht3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opdracht3</title>
+</head>
+<body>
+
+<?php
+
+// Auteur : Mashtaq Thabit
+// functie: maak een string variabele
+
+$var1 = "dit is variabele 1";
+$var2 = "dit is variabele 2";
+
+echo $var1;
+echo "<br>";
+echo $var2;
+
+
+// maak 2 variabele met een getal 
+$getal1 = "10";
+$getal2 = "20";
+
+echo "<br>";
+echo "het getal $getal1 is kleiner dan $getal2 ";
+
+
+?> 
+
+
+    
+</body>
+</html>
